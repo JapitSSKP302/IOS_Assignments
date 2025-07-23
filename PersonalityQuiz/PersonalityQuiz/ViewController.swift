91aa950c07aabf0097774f3a893ef7e91c53962b
JapitSSKP302 <japit0612.be23@chitkara.edu.in>
2025-07-23 11:07:28 +0530

    Finished with Personality Quiz Lab

--- PersonalityQuiz/PersonalityQuiz/ViewController.swift
+++ PersonalityQuiz/PersonalityQuiz/ViewController.swift
@@ -8,11 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func startQuizPressed(_ sender: Any) {
+            performSegue(withIdentifier: "startQuiz", sender: nil)
+    }
+    
+    @IBAction func unwindToIntroVc(segue: UIStoryboardSegue) {}
 
 
 }
