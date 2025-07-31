f9905a59dd468cd64370edeba36323c25e45051f
JapitSSKP302 <japit0612.be23@chitkara.edu.in>
2025-07-31 09:52:32 +0530

    Fixed Errors in Guided Project

--- personality_development/personality_development/AppDelegate.swift
+++ personality_development/personality_development/AppDelegate.swift
@@ -1,8 +1,13 @@
-
+//
+//  AppDelegate.swift
+//  personality_development
+//
+//  Created by Student on 23/07/25.
+//
 
 import UIKit
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,12 +20,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-     
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
 
